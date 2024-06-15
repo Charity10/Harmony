a6d3ab6936ad0264fe2dae8141f6f554419a2ad6
Charity <umercy001@gmail.com>
2024-06-15 06:05:16 +0100

    made changes to the landingpage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
 import '../sass/main.scss'
+
+
 const LandingPage = () => {
   return (
     <div className='landingPage'>
       <div className='landingPage__nav'>
         <h1 className='landingPage__nav--title'>Harmony</h1>
-        <div className="landingPage__nav--btn">Sign in/Sign up</div>
+        <div className="landingPage__nav--btn"><Link className='landingPage__nav--btn-link' to="/signin">Sign in/Sign up</Link></div>
       </div>
       
       <div className="landingPage__slogan">
         <h2>Find your place <br />Find your people</h2>
-        <button className="landingPage__slogan--btn">Get Started</button>
+        <button className="landingPage__slogan--btn"><Link className='landingPage__slogan--btn-link' to="/signin">Get Started</Link></button>
       </div>
+
+      <Outlet />
     </div>
   )
 }
