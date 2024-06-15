f6508b5212603bb47438e5304155107782563aaa
Charity <umercy001@gmail.com>
2024-06-15 04:30:00 +0100

    Modified the landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,12 +5,12 @@ const LandingPage = () => {
     <div className='landingPage'>
       <div className='landingPage__nav'>
         <h1 className='landingPage__nav--title'>Harmony</h1>
-        <div className="landingPage__nav--btn">Sign in</div>
-        <div className="landingPage__nav--btn">Sign up</div>
+        <div className="landingPage__nav--btn">Sign in/Sign up</div>
       </div>
       
       <div className="landingPage__slogan">
-        <h2>Find your place, Find your people</h2>
+        <h2>Find your place <br />Find your people</h2>
+        <button className="landingPage__slogan--btn">Get Started</button>
       </div>
     </div>
   )
